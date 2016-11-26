1e4a8a89587cf909bf29dc0c23cadf322e564495
Torgny Bjers <torgny.bjers@gmail.com>
2016-11-25 23:52:34 -0500

    Installation/uninstall scripts

---
+++ ellipsis.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+pkg.install() {
+  case $(os.platform) in
+    linux)
+      if utils.cmd_exists apt-get; then
+        utils.run_installer https://toolbelt.heroku.com/install-ubuntu.sh
+        heroku --version
+      else
+        utils.run_installer https://toolbelt.heroku.com/install.sh
+        heroku --version
+      fi
+      ;;
+  esac
+}
+
+pkg.uninstall() {
+  case $(os.platform) in
+    linux)
+      if utils.cmd_exists apt-get; then
+        sudo apt-get remove heroku-toolbelt \
+          && rm /etc/apt/sources.list.d/heroku.list
+      else
+        sudo rm -rf /usr/local/heroku \
+          && rm -rf ~/.heroku ~/.local/share/heroku \
+                    ~/.config/heroku ~/.cache/heroku
+      fi
+}
+
